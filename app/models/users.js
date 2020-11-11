17f0f6de5f3379d3ec4c3162ad8acd8bd4861252
lizhihao97 <940166841@qq.com>
2020-11-11 22:56:46 +0800

    update users model

--- app/models/users.js
+++ app/models/users.js
@@ -6,7 +6,8 @@ const userSchema = new Schema({
     __v: {type: Number,select: false},
     username: { type: String, required: true,  unique: true },
     password: { type: String, require: true, select: false },
-    role: { type: Number, enum: [0, 1, 2], default: 2 }
+    role: { type: Number, enum: [0, 1, 2], default: 2 },
+    teacher: { type: Schema.Types.ObjectId, ref: 'User', require: false }
 }, { timestamps: true });
 
 module.exports = model('User', userSchema); 
